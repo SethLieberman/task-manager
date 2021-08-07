e77e43f2b79650d815f8e00b92905fbb88650eb9
Seth Lieberman <seth.lieberman@powerhrg.com>
2021-08-07 13:23:37 -0400

    Add ability to create task using ajax
    When task is created a user should not need to refesh the page
    
    Create a modal for create form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,9 @@ class TasksController < ApplicationController
       if @task.save
         format.html { redirect_to @task, notice: "Task was successfully created." }
         format.json { render :show, status: :created, location: @task }
+        format.js
       else
+        format.js
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
